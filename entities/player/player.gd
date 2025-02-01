c8113e94a795aa97d04ee8a500f5924676b3bef1
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-02-01 13:21:11 +0200

    Implement player position clamping

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,8 +1,25 @@
 class_name Player
 extends CharacterBody2D
 
+const POS_OFFSET: Vector2 = Vector2(64, 64)
+
+var _clamp_pos_h: Vector2 = Vector2.ZERO
+var _clamp_pos_v: Vector2 = Vector2.ZERO
+
 @onready var animation_controller: PlayerAnimaitonController = %Body as PlayerAnimaitonController
 @onready var movement_node: MovementNode = %MovementNode
+
+
+func _ready() -> void:
+	var viewport_size: Vector2 = get_viewport_rect().size
+	_clamp_pos_h.x = POS_OFFSET.x
+	_clamp_pos_h.y = viewport_size.x - POS_OFFSET.x
+	_clamp_pos_v.x = POS_OFFSET.y
+	_clamp_pos_v.y = viewport_size.y - POS_OFFSET.y
+
+
+func _process(_delta: float) -> void:
+	_clamp_position()
 
 
 func _unhandled_key_input(_event: InputEvent) -> void:
@@ -19,3 +36,8 @@ func _unhandled_key_input(_event: InputEvent) -> void:
 			animation_controller.play_move_animation()
 		else:
 			animation_controller.play_stop_animation()
+
+
+func _clamp_position() -> void:
+	position.x = clamp(position.x, _clamp_pos_h.x, _clamp_pos_h.y)
+	position.y = clamp(position.y, _clamp_pos_v.x, _clamp_pos_v.y)

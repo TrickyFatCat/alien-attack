afda635e2ab55ce183eec2d02750a69a4ab361fd
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-02-02 19:28:39 +0200

    Implement damage to projectiles

--- entities/projectiles/projectile.gd
+++ entities/projectiles/projectile.gd
@@ -1,6 +1,21 @@
 class_name Projectile
 extends Area2D
 
+var damage: int = 1:
+	set(value):
+		if value < 0:
+			return
+
+		damage = value
+	get:
+		return damage
+
 
 func _ready() -> void:
 	top_level = true
+	area_entered.connect(_handle_area_entered)
+
+
+func _handle_area_entered(area: Area2D) -> void:
+	if Utils.apply_damage(area, damage):
+		queue_free()
--- src/weapon/weapon.gd
+++ src/weapon/weapon.gd
@@ -2,6 +2,7 @@ class_name Weapon
 extends Node2D
 
 @export var projectile_scene: PackedScene = null
+@export var damage: int = 1
 
 
 func shoot() -> bool:
@@ -11,4 +12,5 @@ func shoot() -> bool:
 	var new_projectile: Projectile = projectile_scene.instantiate()
 	add_child(new_projectile)
 	new_projectile.global_position = global_position
+	new_projectile.damage = damage
 	return true

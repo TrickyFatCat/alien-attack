39bc7cf8702efd68e80dc5bdbb596341276b5104
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-02-02 19:44:12 +0200

    Create explosion animation for enemy

--- entities/enemies/enemy.gd
+++ entities/enemies/enemy.gd
@@ -1,7 +1,12 @@
 class_name Enemy
 extends Area2D
 
+const EXPLOSION_ANIM: String = "explosion"
+
 @export var hitpoints: HitPoints = null
+
+@onready var movement_node: MovementNode = %MovementNode
+@onready var sprite: AnimatedSprite2D = %Sprite
 
 
 func _ready() -> void:
@@ -11,4 +16,20 @@ func _ready() -> void:
 
 
 func _handle_hitpoints_reached_zero() -> void:
-	queue_free()
+	movement_node.process_mode = Node.PROCESS_MODE_DISABLED
+	set_deferred("monitorable", false)
+	set_deferred("monitoring", false)
+
+	if sprite == null:
+		queue_free.call_deferred()
+		return
+
+	sprite.animation_finished.connect(_handle_animation_finished)
+	sprite.play(EXPLOSION_ANIM)
+
+
+func _handle_animation_finished() -> void:
+	if sprite.animation != EXPLOSION_ANIM:
+		return
+
+	queue_free.call_deferred()

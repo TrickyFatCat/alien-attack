72c7a0c1594964c20823c4c09eb759818bb7dc6e
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-02-11 10:45:10 +0200

    Create game over screen

--- entities/user_interface/hud.gd
+++ entities/user_interface/hud.gd
@@ -3,7 +3,9 @@ extends Control
 var _health_icons: Array[Node] = []
 
 @onready var _score_counter: ScoreCounter = %ScoreCounter
-@onready var _score_text: Label = %Text_Score
+@onready var _text_score: Label = %Text_Score
+@onready var _text_final_score: Label = %Text_FinalScore
+@onready var _screen_game_over: Control = %Screen_GameOver
 
 
 func _ready() -> void:
@@ -21,12 +23,20 @@ func _ready() -> void:
 	if player != null && player.hitpoints != null:
 		player.hitpoints.on_hitpoints_decreased.connect(_handle_player_hitpoints_decreased)
 
+	var button_restart: Button = %Button_Restart
+
+	if button_restart != null:
+		button_restart.pressed.connect(_handle_restart_pressed, CONNECT_ONE_SHOT)
+
 
 func _handle_score_increased(new_score: int, _amount: int) -> void:
-	if _score_text == null:
-		return
+	var score_string := str(new_score)
 
-	_score_text.text = str(new_score)
+	if _text_score != null:
+		_text_score.text = score_string
+
+	if _text_final_score != null:
+		_text_final_score.text = score_string
 
 
 func _handle_player_hitpoints_decreased(_amount: int, new_value: int) -> void:
@@ -34,3 +44,10 @@ func _handle_player_hitpoints_decreased(_amount: int, new_value: int) -> void:
 		return
 
 	_health_icons[new_value].visible = false
+
+	if new_value == 0:
+		_screen_game_over.visible = true
+
+
+func _handle_restart_pressed() -> void:
+	get_tree().reload_current_scene()
